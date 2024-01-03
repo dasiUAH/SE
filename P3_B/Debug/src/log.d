9f4d989616a4b64c860f4b9b0ebc3ef6c9e22430
dasiUAH <75973341+dasiUAH@users.noreply.github.com>
2024-01-03 21:57:08 +0100

    Delete P3_B directory

--- P3_B/Debug/src/log.d
+++
@@ -1,4 +0,0 @@
-src/log.o: ../src/log.c /home/atcsol/eclipse-workspace/P3_B/include/log.h \
- /home/atcsol/eclipse-workspace/P3_B/include/riscv_types.h
-/home/atcsol/eclipse-workspace/P3_B/include/log.h:
-/home/atcsol/eclipse-workspace/P3_B/include/riscv_types.h:
